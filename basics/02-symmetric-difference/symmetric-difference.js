f726828bb9358bb8670044c21c24e9c8be3bba5e
Scott D'Alessandro <scott@fullstackacademy.com>
2019-12-20 09:17:40 -0500

    Update symmetric-difference.js

--- basics/02-symmetric-difference/symmetric-difference.js
+++ basics/02-symmetric-difference/symmetric-difference.js
@@ -5,7 +5,7 @@
 // it is not accessible on the "window" object which is where the jasmine `spyOn` function
 // checks for the `difference` function.
 
-// Feel free to change the names of the  paramaters, but if you want your specs to
+// Feel free to change the names of the parameters, but if you want your specs to
 // pass keep the function// declaration syntax.
 
 /* eslint-disable no-unused-vars */
